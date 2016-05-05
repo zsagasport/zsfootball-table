954a9bae55604c7ec4d3ca4ea27fe089f0ef766e
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-05-05 09:58:21 +0200

    Adds test

---
+++ test/ZsFootballTable.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import ZsFootballTable from '../src/ZsFootballTable';
+
+describe('ZsFootballTable', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
